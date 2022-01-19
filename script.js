8bd66f798a06e4629e2edde3c505720957a29ce9
NathanBrownWebDev <94125423+NathanBrownWebDev@users.noreply.github.com>
2022-01-19 16:30:32 -0600

    cloud save - first push

---
+++ script.js
@@ -0,0 +1,36 @@
+//load top of webpage on reload
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
+//hide/show nav bar on scroll
+const navCheckbox = document.querySelector(".nav-checkbox");
+const navHeader = document.querySelector('.header');
+
+let prevScrollpos = window.pageYOffset;
+
+//close nav menu after click on link
+function closeMenu(){
+    navCheckbox.checked = false; 
+}
+const navListsItemLinks = document.querySelectorAll(".nav__list__item__link");
+navListsItemLinks.forEach(link => {
+    link.addEventListener("click", closeMenu);
+})
+//hide or show nav bar based on window scroll
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos || window.pageYOffset <= 5) {
+    navHeader.classList.add("header-show");
+    navHeader.classList.remove("header-hide");
+//   } else if (for (let link of navListsItemLinks) {link.clicked == true})  {
+//     navHeader.classList.add("header-hide");
+//     navHeader.classList.remove("header-show");
+//     navCheckbox.checked = false;
+//     console.log(link);
+  } else {
+    navHeader.classList.add("header-hide");
+    navHeader.classList.remove("header-show");
+    navCheckbox.checked = false; 
+  }
+  prevScrollpos = currentScrollPos;
+}
